747a7a591c07af903966d9d37863d757eb4c06b5
ocarballido <ocarballido@gmail.com>
2019-12-21 19:25:51 +0100

    adding js code

--- card-moove/js/app.js
+++ card-moove/js/app.js
@@ -1,83 +1,37 @@
 $(document).ready(function(){
     //Variables
-    var date;
-    var seconds;
-    var minutes;
-    var hours;
-    var tenth;
-    var thousandths;
+    var windows = $(window)
+		windowsHeight = windows.height()
+        windowsWidth = windows.width()
+        maxShadows = [10, 20, 30, 80];
+        
+    $( document ).on( "mousemove", function( event ) {
+        var mouseOnX = event.pageX;
+        var mouseOnY = event.pageY;
+        $('.card').each(function() {
+            var cardWidth = $(this).outerWidth();
+            var cardHeight = $(this).outerHeight();
+            var offset = $(this).offset();
+            var cardCenterX = offset.left + (cardWidth / 2);
+            var cardCenterY = offset.top + (cardHeight / 2);
+            var maxAngle = 60;
+            var distanceX = mouseOnX - cardCenterX;
+            var distanceY = mouseOnY - cardCenterY;
+            var ratioDistanceX = (maxAngle * distanceX) / windowsWidth;
+            var ratioDistanceY = (maxAngle * distanceY) / windowsHeight;
+            var shadow1X = ((maxShadows[0] * distanceX) / windowsWidth) * -1;
+            var shadow2X = ((maxShadows[1] * distanceX) / windowsWidth) * -1;
+            var shadow3X = ((maxShadows[2] * distanceX) / windowsWidth) * -1;
+            var shadow4X = ((maxShadows[3] * distanceX) / windowsWidth) * -1;
+            var shadow1Y = ((maxShadows[0] * distanceY) / windowsHeight) * -1;
+            var shadow2Y = ((maxShadows[1] * distanceY) / windowsHeight) * -1;
+            var shadow3Y = ((maxShadows[2] * distanceY) / windowsHeight) * -1;
+            var shadow4Y = ((maxShadows[3] * distanceY) / windowsHeight) * -1;
 
-    function clock() {
-        date = new Date();
-        tenth = date.getMilliseconds();
-        seconds = date.getSeconds();
-        minutes = date.getMinutes();
-        hours = date.getHours();
-
-        //$('.test').text(Math.round(tenth / 100));
-
-        for(var i = 0; i <= 9; i++) {
-            // Tenth of a seconds
-            if(Math.round(tenth / 100)%10 == i) {
-                $('.tenth .number:eq(0) .row-led').removeClass('on');
-                $('.tenth .number:eq(0) .row-led.led-' + i + '').addClass('on');
-            }
-
-            // Seconds
-            if(seconds%10 == i) {
-                $('.seconds .number:eq(1) .row-led').removeClass('on');
-                $('.seconds .number:eq(1) .row-led.led-' + i + '').addClass('on');
-            }
-            if(Math.floor(seconds/10) == i) {
-                $('.seconds .number:eq(0) .row-led').removeClass('on');
-                $('.seconds .number:eq(0) .row-led.led-' + i + '').addClass('on');
-            }
-
-            // Minutes
-            if(minutes%10 == i) {
-                $('.minutes .number:eq(1) .row-led').removeClass('on');
-                $('.minutes .number:eq(1) .row-led.led-' + i + '').addClass('on');
-            }
-            if(Math.floor(minutes/10) == i) {
-                $('.minutes .number:eq(0) .row-led').removeClass('on');
-                $('.minutes .number:eq(0) .row-led.led-' + i + '').addClass('on');
-            }
-
-            // Hours
-            if(hours%10 == i) {
-                $('.hours .number:eq(1) .row-led').removeClass('on');
-                $('.hours .number:eq(1) .row-led.led-' + i + '').addClass('on');
-            }
-            if(Math.floor(hours/10) == i) {
-                $('.hours .number:eq(0) .row-led').removeClass('on');
-                $('.hours .number:eq(0) .row-led.led-' + i + '').addClass('on');
-            }
-        }
-    }
-    setInterval(clock, 100);
-
-    $('.btn-group-toggle label').each(function() {
-        $(this).on('click', function() {
-            var optionclick = $(this).find('input').attr('id');
-            switch(optionclick) {
-                case "hours":
-                    $('.hours').toggleClass('d-none');
-                    $('.divider-minutes').toggleClass('d-none d-flex');
-                    break;
-                case "minutes":
-                    $('.minutes').toggleClass('d-none');
-                    $('.divider-minutes').toggleClass('d-none d-flex');
-                    break;
-                case "seconds":
-                    $('.seconds').toggleClass('d-none');
-                    $('.divider-seconds').toggleClass('d-none d-flex');
-                    break;
-                case "tenth":
-                    $('.tenth').toggleClass('d-none');
-                    $('.divider-tenth').toggleClass('d-none d-flex');
-                    break;
-            }
-            console.log(optionclick);
+            $(this).css({
+                'transform':    'perspective(1500px) rotateY(' + ratioDistanceX + 'deg) rotateX(' + ratioDistanceY * -1 + 'deg)',
+                "box-shadow": "" + shadow1X + "px " + shadow1Y + "px 2px rgba(0, 0, 0, 0.077)," + shadow2X + "px " + shadow2Y + "px 6px rgba(0, 0, 0, 0.089)," + shadow3X + "px " + shadow3Y + "px 12px rgba(0, 0, 0, 0.093)," + shadow4X + "px " + shadow4Y + "px 24px rgba(0, 0, 0, 0.094)"
+            });
         });
     });
 });
